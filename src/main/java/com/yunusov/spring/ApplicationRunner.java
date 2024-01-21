7fb26ef728325f7290e62d6f09cb6c5981a4be39
Daler Yunusov <daleryunusov2001@gmail.com>
2024-01-21 13:23:53 +0300

    XML-based Configurations, 2

--- src/main/java/com/yunusov/spring/ApplicationRunner.java
+++ src/main/java/com/yunusov/spring/ApplicationRunner.java
@@ -5,20 +5,13 @@ import com.yunusov.spring.database.repository.CompanyRepository;
 import com.yunusov.spring.database.repository.UserRepository;
 import com.yunusov.spring.ioc.Container;
 import com.yunusov.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var container = new Container();
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-//        var userService = container.get(UserRepository.class);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("pool1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 
 }
